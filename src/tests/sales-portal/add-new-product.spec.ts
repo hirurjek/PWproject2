21fda187d52d6bffd4beec4e14287c31c334aaa7
Yuriy Shevtsov <yuriy.shevtsov@geozilla.com>
2025-11-01 17:34:49 +0000

    copy initial project

---
+++ src/tests/sales-portal/add-new-product.spec.ts
@@ -0,0 +1,52 @@
+import test, { expect } from "@playwright/test";
+import { credentials } from "config/env";
+import { NOTIFICATIONS } from "data/salesPortal/notifications";
+import { generateProductData } from "data/salesPortal/products/generateProductData";
+// import { MANUFACTURERS } from "data/salesPortal/products/manufacturers";
+// import { IProduct } from "data/types/product.types";
+import { HomePage } from "ui/pages/home.page";
+import { AddNewProductPage } from "ui/pages/products/addNewProduct.page";
+import { ProductsListPage } from "ui/pages/products/productList.page";
+
+// const productData: IProduct = {
+//   name: "Product" + Date.now(),
+//   manufacturer: MANUFACTURERS.GOOGLE,
+//   price: 1,
+//   amount: 2,
+//   notes: "test notes",
+// };
+
+test.describe("[Sales Portal] [Products]", async () => {
+  test("Add new product", async ({ page }) => {
+    const homePage = new HomePage(page);
+    const productsListPage = new ProductsListPage(page);
+    const addNewProductPage = new AddNewProductPage(page);
+
+    //login page
+    const emailInput = page.locator("#emailinput");
+    const passwordInput = page.locator("#passwordinput");
+    const loginButton = page.locator("button[type='submit']");
+
+    await homePage.open();
+
+    await expect(emailInput).toBeVisible();
+    await emailInput.fill(credentials.username);
+    await passwordInput.fill(credentials.password);
+    await loginButton.click();
+
+    await homePage.waitForOpened();
+    await homePage.clickOnViewModule("Products");
+    await productsListPage.waitForOpened();
+    await productsListPage.clickAddNewProduct();
+    await addNewProductPage.waitForOpened();
+    const productData = generateProductData();
+    await addNewProductPage.fillForm(productData);
+    await addNewProductPage.clickSave();
+    await productsListPage.waitForOpened();
+    await expect(productsListPage.toastMessage).toContainText(NOTIFICATIONS.PRODUCT_CREATED);
+    await expect(productsListPage.tableRowByName(productData.name)).toBeVisible();
+  });
+});
+
+
+//teardown
